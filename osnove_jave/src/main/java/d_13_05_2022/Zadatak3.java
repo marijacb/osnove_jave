e567e8bfe4ee6b3e6f3d15629ae4f5488ecc8505
Marija Barjaktarevic <marija.barjaktarevic1@gmail.com>
2022-05-16 09:18:48 +0200

    Domaci zadatak 13.05.2022. Zadatak 3

--- osnove_jave/src/main/java/d_13_05_2022/Zadatak3.java
+++ osnove_jave/src/main/java/d_13_05_2022/Zadatak3.java
@@ -1,2 +1,40 @@
-package d_13_05_2022;public class Zadatak3 {
+package d_13_05_2022;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+//Napisati program koji ima niz brojeva duzine 10.
+// Niz je fiksnih vrednosti (ne u nosi ih korisnik)
+// a zatim se od korisnika ucitava pozicija elementa koji zeli da izbaci iz niza.
+// Pre i nakon izbacivanja ispisati niz na ekranu.
+// Ukoliko korisnik unese poziciju van opsega niza, ispisati poruku o gresci.
+//Commitujte zadatak i pushujte na github sa porukom Domaci 13.05.2022 Zadatak 3 brisanje iz niza“
+//Primer izvrsenja 1:
+//Niz je: 1,3,5,6,7,1,3,4,2,9
+//Unesite poziciju za izbacivanje: 5
+//Niz nakon brisanja je: 1,3,5,6,7,3,4,2,9
+public class Zadatak3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> niz = new ArrayList<Integer>();
+        Random random = new Random();
+        Scanner s = new Scanner(System.in);
+        System.out.println("Niz je: ");
+        for (int i = 0; i < 10; i++) {
+            int randomBroj = random.nextInt(10);
+            niz.add(randomBroj);
+        }
+        for (int i = 0; i < niz.size(); i++) {
+            System.out.print(niz.get(i) + ",");
+        }
+        System.out.println(" ");
+        System.out.print("Unesi poziciju za izbacivanje: ");
+        int pozicija = s.nextInt();
+        System.out.print("Niz nakon brisanja je");
+        for (int i = 0; i < niz.size(); i++) {
+            niz.remove(pozicija);
+            System.out.print(niz.get(i) + ",");
+        }
+
+    }
 }
--- osnove_jave/src/main/java/d_13_05_2022/zadatak1.java
+++ osnove_jave/src/main/java/d_13_05_2022/zadatak1.java
@@ -1,2 +1,51 @@
-package d_13_05_2022;public class zadatak1 {
-}
+package d_13_05_2022;
+
+import com.sun.source.tree.NewArrayTree;
+
+import javax.naming.PartialResultException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+//Napisati program koji ima niz brojeva od 25 elemenata, koji izgleda kao tabela 5x5. Niz je fiksnih vrednosti
+// (ne unosi ih korisnik) zatim korisnik unosi poziciju za koju se racuna suma.
+//Suma za poziciju se racuna tako sto se na vrednost tog polja dodaju
+// vrednosti elemenata iznad, ispod, sleve i sdesne strane tog elementa
+// (Ukoliko ti elementi postoje, vodite racuna da ne izadjete van opsega niza)
+//Za racunanje sume, nije potrebna petlja!
+//
+//U projektu kreirati paket za ovaj domaci d13_05_2022 i
+// u njemu klasu Zadatak1.java i postavite tekst zadataka u komentaru a zatim
+// commit-ujte promenu i push-ujte na github sa porukom “Domaci 13.05.2022 Zadatak 1”
+public class zadatak1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> niz = new ArrayList<Integer>();
+        Random random = new Random();
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < 25; i++) {
+            int randomBroj = random.nextInt(10);
+            niz.add(randomBroj);
+        }
+        for (int i = 0; i < niz.size(); i++) {
+            System.out.printf(niz.get(i) + ",");
+            if (i % 5 == 4) {
+                System.out.println();
+            }}
+
+        for (int i = 0; i < niz.size(); i++) {
+            System.out.println("unesi poziciju");
+            int p = s.nextInt();
+            System.out.println("suma je");
+            if(p>0){
+                int suma = (p-0)+(p+1)+(p+5)+(p-5);
+                System.out.println(suma);
+            }
+
+
+        }
+
+
+
+        }
+        }
+
