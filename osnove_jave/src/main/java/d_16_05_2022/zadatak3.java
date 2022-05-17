644e7ec449b2bcf1288e8aec82ea5b636ab2b7bd
Marija Barjaktarevic <marija.barjaktarevic1@gmail.com>
2022-05-17 18:20:54 +0200

    Domaci 16.05.2022. zadatak3

--- osnove_jave/src/main/java/d_16_05_2022/zadatak3.java
+++ osnove_jave/src/main/java/d_16_05_2022/zadatak3.java
@@ -1,2 +1,27 @@
-package d_16_05_2022;public class zadatak3 {
+package d_16_05_2022;
+//Zadatak
+//Napisati metodu koja stampa podatke o korisniku u formatu:
+//JMBG: [jmbg]
+//Ime: [ime]
+//Prezime: [prezime]
+//God. rodjenja: [god]
+//Aktivan: [true/false]
+//Metoda prima jmbg, ime, prezime, god rodjenja i da li je aktivan kao parametre metode.
+//}
+public class zadatak3 {
+
+public static void main(String[]args){
+    podaciKorisnika("1704990738557", "Marija", "Barjaktarevic", "1990", true);
+
+        }
+        static void podaciKorisnika(String jmbg, String ime, String prezime, String godRodjenja, boolean aktivan){
+            System.out.println("jmbg: " + jmbg);
+            System.out.println("ime: " + ime);
+            System.out.println("prezime: " + prezime);
+            System.out.println("godina rodjenja: " + godRodjenja);
+            System.out.println("Aktivan: " + aktivan);
+    }
+
+
+
 }
