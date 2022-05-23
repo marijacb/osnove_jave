671ff8c19d700a30394c24b0acf746cbc720ddd1
Marija Barjaktarevic <marija.barjaktarevic1@gmail.com>
2022-05-23 16:14:21 +0200

    Domaci 18.05.2022. Proizvod, ProizvodMain, SmartAirCondition

--- osnove_jave/src/main/java/d_18_05_2022/SmartAirConditioning.java
+++ osnove_jave/src/main/java/d_18_05_2022/SmartAirConditioning.java
@@ -1,2 +1,23 @@
-package d_18_05_2022;public class SmartAirConditioning {
+package d_18_05_2022;
+
+//Kreirati klasu SmartAirConditioning koja ima:
+//atribut za marku klime (npr: Samsung, Galanz, Gree, …)
+//atribut za izabranu temperaturu (minimum je 16, maksimum 35)
+//atribut za mod (hladi/greje)
+//metodu za stampu - stampa u formatu: Proizvoljno :)
+//U glavnom programu kreirati minimum 2 objekta klase
+// SmartAirConditioning i za svaki od njih pozvati metodu za stampu
+public class SmartAirConditioning {
+    public String marka;
+    public double temperaturaMax;
+    public double temperaturaMin;
+    public String modH;
+    public String modG;
+
+    public void Stampa() {
+        System.out.print("Marka klime " + marka + " moze da " + modH + " " + temperaturaMin);
+        System.out.println(" i da " + modG + " " + temperaturaMax);
+    }
+
+
 }
--- osnove_jave/src/main/java/d_18_05_2022/SmartAirConditioningMain.java
+++ osnove_jave/src/main/java/d_18_05_2022/SmartAirConditioningMain.java
@@ -1,2 +1,20 @@
-package d_18_05_2022;public class SmartAirConditioningMain {
+package d_18_05_2022;
+
+public class SmartAirConditioningMain {
+    public static void main(String[] args) {
+        SmartAirConditioning klima = new SmartAirConditioning();
+        klima.marka = "samsung";
+        klima.temperaturaMax = 35;
+        klima.temperaturaMin = 16;
+        klima.modG = "greje";
+        klima.modH = "hladi";
+        klima.Stampa();
+
+        klima.marka = "LG";
+        klima.temperaturaMax = 35;
+        klima.temperaturaMin = 16;
+        klima.modG = "greje";
+        klima.modH = "hladi";
+        klima.Stampa();
+    }
 }
