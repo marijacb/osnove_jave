63e9284c5d63f0d85b858b76bb2fb9c95961ea45
Marija Barjaktarevic <marija.barjaktarevic1@gmail.com>
2022-05-23 14:31:39 +0200

    Domaci 18.05.2022. Proizvod

--- osnove_jave/src/main/java/d_18_05_2022/Proizvod.java
+++ osnove_jave/src/main/java/d_18_05_2022/Proizvod.java
@@ -1,2 +1,21 @@
-package d_18_05_2022;public class zadatak1 {
-}
+package d_18_05_2022;
+//Napisati klasu Proizvod koja ima atribute
+//naziv proizvoda (String)
+//cenu proizvoda (double)
+//težinu proizvoda u gramima. (double)
+//	i metode:
+//stampaj - stampa podatke proizvoda u formatu {{naziv}}, {{cena}}, {{tezina}}
+//
+//
+//U glavnom programu kreirati minimum 2 proizvoda i za svaki od njih pozvati metodu za stampu.
+public class Proizvod {
+    public String naziv;
+    public double cena;
+    public double tezinaG;
+
+
+
+public void Stampaj(){
+    System.out.println("Naziv proizvoda " + naziv + " cena proizvoda " + cena + " tezina proizvoda " + tezinaG);
+}}
+
